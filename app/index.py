aa39f01d3818481c3fa908d6f7e5db66f9258950
phamphat.230905@gmail.com <phamphat.230905@gmail.com>
2025-11-29 15:47:22 +0700

    Change nav bar

--- app/index.py
+++ app/index.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+@app.route('/about-us')
+def about_us():
+    return render_template("about-us.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
+
