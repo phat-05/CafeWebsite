917c368a5e5eac12b1ee33f09a5ab00307c7c3a6
phamphat.230905@gmail.com <phamphat.230905@gmail.com>
2025-12-09 16:59:32 +0700

    Add carousel banners

--- app/index.py
+++ app/index.py
@@ -1,11 +1,21 @@
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    with open("./static/data/banner.json") as f:
+        banners = json.load(f)
+    return render_template("index.html", banners=banners)
 
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+@app.route('/about-us')
+def about():
+    return render_template("about-us.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
